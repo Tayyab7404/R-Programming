87f9524743676dff2ffdea925e0ca7b7959fdf79
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2023-03-17 19:53:20 +0530

    Update 1 a Bar Plot 1.R

--- 01 Graphical Representation of Data/1 a Bar Plot 1.R
+++ 01 Graphical Representation of Data/1 a Bar Plot 1.R
@@ -4,10 +4,10 @@ score = c(94, 69, 85, 79, 99)
 
 names = c("Roshan", "Karthik", "Sufiyan", "Neeraj", "Tayyab")
 
-barplot(score, 
-        xlab = "Names", 
-        ylab = "Score", 
-        ylim = c(0,100), 
-        main = "Scores", 
+barplot(score,  
         names.arg = names, 
+        xlab = "Names", 
+        ylab = "Score",  
+        main = "Scores",
+        ylim = c(0,100),
         col = rainbow(length(names)))
